592e0f42502924e0c2dcf16c1de58344f3ccb8c9
jojoldu <jojoldu@gmail.com>
2021-08-05 10:04:22 +0900

    typeORM & dotenv 환경 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(databaseConfig.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

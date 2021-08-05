ee8e0733093433bce3dee86afa0556abe2f9001e
jojoldu <jojoldu@gmail.com>
2021-08-05 09:37:39 +0900

    nestjs 초기 세팅 & datadog 만 연결 테스트

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './user.controller';
-import { UserService } from './service/UserService';
-import { UserRepository } from './entity/user/UserRepository';
-import { DatabaseModule } from './DatabaseModule';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), DatabaseModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService, UserRepository],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
